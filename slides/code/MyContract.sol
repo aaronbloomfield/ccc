93e4fa4fb5ca0b09564e446fc9641a4af360d66d
Aaron Bloomfield <aaron@virginia.edu>
2022-10-05 12:41:37 -0400

    Updates for today's lecture

---
+++ slides/code/MyContract.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+// This example is just to show the compiled bytecode and opcodes in the
+// solidity slide set.
+
+pragma solidity ^0.8.16;
+contract MyContract {
+    uint public i = 1 + 2 * 3 - 4;
+}
